383def53dadaf2504a7542684f06b5ff34fb4359
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-10-17 11:40:22 -0400

    help

--- resources/views/graficador.blade.php
+++ resources/views/graficador.blade.php
@@ -39,9 +39,8 @@
                     <p class="text-2xl font-bold text-left py-2 px-2">
                         {{ $diagrama->nombre }}
                     </p>
-
-                    <button class="px-1 m-1" type="button" data-modal-target="small-modal"
-                        data-modal-toggle="small-modal">
+                    <button data-modal-target="extralarge-modal" data-modal-toggle="extralarge-modal"
+                        class="block text-black font-medium rounded-lg text-sm px-5 py-2.5 text-center " type="button">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -51,10 +50,10 @@
                 </div>
                 <div class="flex flex-col items-center justify-center w-full h-full px-2 pb-14">
                     <textarea name="" id="editor" class="w-full h-full " cols="30" rows="18">{{ $diagrama->markdown }}</textarea>
-                    <button class="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+                    {{-- <button class="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
                         onclick="exportar()">
                         Exportar
-                    </button>
+                    </button> --}}
                 </div>
             </div>
 
@@ -69,19 +68,17 @@
             </div>
 
         </div>
-        <div id="small-modal" tabindex="-1"
+        <div id="extralarge-modal" tabindex="-1"
             class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative w-full max-w-md max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                            Small modal
+            <div class="relative w-full max-w-xl max-h-full">
+                <div class="relative bg-white rounded-lg shadow ">
+                    <div class="flex items-center justify-between p-5 border-b rounded-t ">
+                        <h3 class="text-xl font-medium text-gray-900 ">
+                            Sintaxis
                         </h3>
                         <button type="button"
-                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-hide="small-modal">
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center "
+                            data-modal-hide="extralarge-modal">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -91,27 +88,127 @@
                         </button>
                     </div>
                     <!-- Modal body -->
-                    <div class="p-6 space-y-6">
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for
-                            its citizens, companies around the world are updating their terms of service agreements to
-                            comply.
-                        </p>
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May
-                            25 and is meant to ensure a common set of data rights in the European Union. It requires
-                            organizations to notify users as soon as possible of high-risk data breaches that could
-                            personally affect them.
-                        </p>
+                    <div class=" p-6">
+
+                        <div class="relative overflow-x-auto">
+                            <table class="w-full text-sm text-left text-gray-500 ">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Comando
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Ejemplo / Resultado
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b ">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                            participant
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <img src="{{ asset('img/participant.png') }}" alt="" class="w-32">
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b ">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                            actor
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <img src="{{ asset('img/actor.png') }}" alt="" class="w-32">
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b ">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                            <span class="font-bold text-base">
+                                                Mensajes <br>
+                                            </span>
+                                            <span class="font-semibold">
+                                                [Actor][Arrow][Actor]:Message text
+                                            </span><br>
+                                            Lista de flechas: <br>
+                                            <ul class="text-base">
+                                                <li>->: get </li>
+                                                <li>-->: get </li>
+                                                <li>->>: get </li>
+                                                <li>-->>: get </li>
+                                                <li>-x: get</li>
+                                                <li>--x: get</li>
+                                                <li>-): get</li>
+                                                <li>--): get</li>
+                                            </ul>
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <img src="{{ asset('img/message.png') }}" alt="" class="w-80">
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b ">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                            <span class="font-bold text-base">
+                                                Activacion <br>
+                                            </span>
+                                            Zalazar->>Nahuel: Get <br>
+                                            activate Nahuel <br>
+                                            Nahuel-->>Zalazar: Get <br>
+                                            deactivate Nahuel <br>
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <img src="{{ asset('img/activate.png') }}" alt="" class="w-44">
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b ">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                            <span class="font-bold text-base">
+                                                Estructuras de control <br>
+                                            </span>
+                                            Nahuel->Zalazar: Get <br>
+                                            loop Ciclo <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            end <br><br>
+                                            alt Alternativa <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            else <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            end <br><br>
+                                            par Nahuel to Zalazar <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            and Nahuel to Zalazar <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            end <br> <br>
+                                            critical Critico <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            option Network timeout <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            option Credentials rejected <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            end <br> <br>
+                                            break break <br>
+                                            Nahuel-->Zalazar: Post! <br>
+                                            end <br>
+
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <img src="{{ asset('img/control.png') }}" alt="" class="w-">
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                     <!-- Modal footer -->
-                    <div
-                        class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="small-modal" type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b ">
+                        {{-- <button data-modal-hide="extralarge-modal" type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">I
                             accept</button>
-                        <button data-modal-hide="small-modal" type="button"
-                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                        <button data-modal-hide="extralarge-modal" type="button"
+                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Decline</button> --}}
                     </div>
                 </div>
             </div>
@@ -223,8 +320,6 @@
             editor.value = markdown;
             await drawDiagram(markdown);
         });
-
-
 
         setInterval(function() {
             let id = {{ $diagrama->id }};
